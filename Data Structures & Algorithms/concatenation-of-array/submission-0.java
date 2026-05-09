d0d46ae3e908d751bf55f657fd98f535aa89a12a
Arushi Patel <arushipatel6927@gmail.com>
2026-05-09 21:59:56 +0530

    Bulk sync: 3 submissions

---
+++ Data Structures & Algorithms/concatenation-of-array/submission-0.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int[] getConcatenation(int[] nums) {
+        int n = nums.length;
+        int[] ans = new int[2*n];
+        int k=0;
+        for(int i = 0 ; i < nums.length ; i++){
+            ans[i] = nums[i];
+            k++;
+        }
+        for(int i = 0 ; i < nums.length ; i++){
+            ans[k] = nums[i];
+            k++;
+        }
+        return ans;
+    }
+}
